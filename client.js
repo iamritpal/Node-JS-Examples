c3d8e1bcd6f253f480f4d9c98041446976c76bc8
Amritpal Singh <amritpal@singh.email>
2017-08-02 21:19:02 -0400

    first commit

---
+++ client.js
@@ -0,0 +1,35 @@
+var net = require('net');
+
+//var HOST = '127.0.0.1';
+var HOST = '192.168.1.153';
+var PORT = 6969;
+
+var debug1 = 0;
+
+var client = new net.Socket();
+
+client.connect(PORT, HOST, function() {
+
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
+    client.write('I am Chuck Norris!');
+
+});
+
+// Add a 'data' event handler for the client socket
+// data is what the server sent to this socket
+client.on('data', function(data) {
+
+    console.log('DATA: ' + data);
+    debug1 = debug1 + 1;
+    client.write('I am Amritpal Singh!' + debug1);
+
+    // Close the client socket completely
+    //client.destroy();
+    
+});
+
+// Add a 'close' event handler for the client socket
+client.on('close', function() {
+    console.log('Connection closed');
+});
